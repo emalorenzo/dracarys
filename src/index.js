f6ef16354737cdf9cc3c26846c613e035e9a8c99
Ema Lorenzo <aemalorenzo@gmail.com>
2019-05-14 13:35:59 -0300

    Checkpoint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-module.exports = () => console.log("Hello World!");
+#!/usr/bin/env node
+
+const chalk       = require('chalk');
+const clear       = require('clear');
+const figlet      = require('figlet');
+
+const files = require('../lib/files');
+const generator = require('../lib/generator');
+
+console.log(
+  chalk.red(
+    figlet.textSync('Dracarys', { horizontalLayout: 'full' })
+  )
+);
+
+// if (files.directoryExists('.git')) {
+//   console.log(chalk.red('Already a git repository!'));
+//   process.exit();
+// }
+
+const inquirer  = require('../lib/inquirer');
+
+async function run() {
+  console.log(files.getCurrentDirectoryBase());
+  const { operation } = await inquirer.askOperation();
+  console.log(operation);
+
+  switch(operation) {
+    case 'function-component': generator.createFile();
+    case 'class-component': generator.createFile();
+    case 'screen': handleGenerateScreen();
+  }
+}
+
+async function handleGenerateScreen() {
+  const { features } = await inquirer.askScreenFeatures();
+  console.log(features);
+}
+
+run();
---
+++ templates/functionComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const COMPONENT_NAME = () => {
+  return (
+    <View>
+      <Text>COMPONENT_NAME</Text>
+    </View>
+  );
+}
+
+export default COMPONENT_NAME;
